d7a44a7ec3bfcb55950ae54d063980a97e7585e9
Allison Castaneda <allraec@yahoo.com>
2021-03-06 19:02:32 -0600

    reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,28 @@
 
 export const initialState = {
+    smurfs: [],
+    isLoading: false,
+    error: ""
 }
 
-const reducer = ()=>{
+const reducer = (state = initialState, action)=>{
+    switch(action.type){
+        case("FETCH_START"):
+            return {...state, isLoading: true};
+        case("FETCH_SUCCESS"):
+            return {...state, smurfs: action.payload, isLoading: false};
+        case("FETCH_FAIL"):
+            return state;
+        case("ADD_SMURF"):
+            return {
+                ...state,
+                smurfs: [...state.smurfs, action.payload]
+            };
+        case("ADD_ERROR"):
+            return {...state, error: action.payload};
+        default:
+            return state;
+    }
 }
 
 export default reducer;
