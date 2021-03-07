f703a6e281260b180e0aac868c41958eae55136f
Allison Castaneda <allraec@yahoo.com>
2021-03-06 19:03:53 -0600

    connected fetch smurf api thunk action

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import React, { Component } from "react";
 import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
 import Header from './components/Header';
+import {fetchSmurfs} from './actions/index'
+import {connect} from 'react-redux';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
 class App extends Component {
+  componentDidMount(){
+    this.props.fetchSmurfs();
+  }
   render() {
     return (
       <div className="App">
@@ -22,7 +27,8 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = {fetchSmurfs}
+export default connect(null, mapDispatchToProps)(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
